28004041a6ac755ad1a1179f75d998eb5b6e4b8d
Ankita Kalangutkar <ankitakalangutkar@Ankitas-MacBook-Pro-2.local>
2019-07-21 18:06:38 +0530

    Initial project setup

---
+++ SortingAndSearchingAlgorithms/SortingAndSearchingAlgorithms/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SortingAndSearchingAlgorithms
+//
+//  Created by Ankita Kalangutkar on 21/07/19.
+//  Copyright © 2019 Ankita Kalangutkar. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
